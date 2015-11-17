ec9f089a96d7bfd9a4bd33c1e740d5f89b9122f3
sasar103 <Sanjoli.Saraswat@uni-duesseldorf.de>
2015-11-17 21:27:56 +0100

    Done

--- statistics.cxx
+++ statistics.cxx
@@ -2,20 +2,32 @@
 #include <iostream>
 #include <cstdlib>
 
-using namespace std;
-//--------------------------------------------------
-// plenty of lines here
-//--------------------------------------------------
-
-int main(){
-   const int N = 100;
-   double p[N];
-   double mean, var;
-
-   // Some lines here....
-
-   cout << "Mean = " << mean << endl;
-   cout << "Variance = " << var << endl;
-
-   return 0;
+usingspace std;
+ 
+void RandNum(double* x, int N){
+  for(int i=0; i<N; i++)
+  x[i] = rand()/double(RAND_MAX);
 }
+
+
+void stat(double* p, double& mean, double& var, int N){
+  mean = 0;
+  for(int i=0; i<N; i++)	
+     mean += p[i]/N;
+     var = 0;
+   for(int i=0; i<N; i++)	
+   var = ((p[i]-mean)*(p[i]-mean))/N;
+
+
+ int main(){
+    const int N = 100;
+    double p[N];
+    double mean, var;
+ 
+ RandNum(p, N);
+  stat(p, mean, var, N)  
+    cout << "Mean = " << mean << endl;
+    cout << "Variance = " << var << endl;
+ 
+    return 0;
+ }
