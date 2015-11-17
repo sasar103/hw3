d2bc494cc7b88aa2be72481af0e5ee2702577e7f
sasar103 <Sanjoli.Saraswat@uni-duesseldorf.de>
2015-11-17 21:37:41 +0100

    Done Hw3

--- statistics.cxx
+++ statistics.cxx
@@ -10,24 +10,26 @@ void RandNum(double* x, int N){
 }
 
 
-void stat(double* p, double& mean, double& var, int N){
-  mean = 0;
+void statistics(double* p, double& m, double& v, int N){
+  m= 0;
   for(int i=0; i<N; i++)	
-     mean += p[i]/N;
-     var = 0;
-   for(int i=0; i<N; i++)	
-   var = ((p[i]-mean)*(p[i]-mean))/N;
+  m+= p[i]/N;
+  v= 0;
+  for(int i=0; i<N; i++)	
+  v= ((p[i]-m)*(p[i]-m))/N;
+}
 
 
  int main(){
     const int N = 100;
     double p[N];
-    double mean, var;
- 
+    double m;
+    Double v;
+
  RandNum(p, N);
-  stat(p, mean, var, N)  
-    cout << "Mean = " << mean << endl;
-    cout << "Variance = " << var << endl;
+ statistics(p, m, v, N)  
+    cout << "Mean = " << m << endl;
+    cout << "Variance = " << v<< endl;
  
     return 0;
  }
